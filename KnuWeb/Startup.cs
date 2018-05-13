4ba93a5a2508e59a912aa0deb03834d3db8f93fa
Igor Konobas <kois9911@gmail.com>
2018-05-13 10:04:17 +0300

    Add project files.

---
+++ KnuWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KnuWeb.Startup))]
+namespace KnuWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
