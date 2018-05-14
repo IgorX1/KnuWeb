c90d3926c8230c83e216f227a9131c726af78197
Igor Konobas <kois9911@gmail.com>
2018-05-14 20:53:33 +0300

    departments are shown now. Hooray!!!!!

---
+++ EmployeeEF/DEPARTMENT.partial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeEF
+{
+    [MetadataType(typeof(DEPARTMENTMD))]
+    partial class DEPARTMENT
+    {
+        public class DEPARTMENTMD
+        {
+            [HiddenInput(DisplayValue = false)]
+            public int ID { get; set; }
+
+            [Display(Name = "Факультет")]
+            public string D_NAME { get; set; }
+        }
+    }
+}
---
+++ KnuWeb/Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeEF;
+
+namespace KnuWeb.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private KNUDBEntities ctx = new KNUDBEntities();
+        // GET: Department
+        public ActionResult Index()
+        {
+
+            return View(ctx.DEPARTMENT.ToList());
+        }
+    }
+}
