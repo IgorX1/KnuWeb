580c32e59d3777daf79b77eb6dc2c4e0455193dd
Igor Konobas <kois9911@gmail.com>
2018-05-14 21:11:06 +0300

    Сторінку з кафедрами додано (я ніфіга не в'їхав чого там воно автоматично з'єднує по форін кі --> holy magic!)

---
+++ EmployeeEF/CATHEDRA.partial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeEF
+{
+    [MetadataType(typeof(CATHEDRAMD))]
+    partial class CATHEDRA
+    {
+        public class CATHEDRAMD
+        {
+            [HiddenInput(DisplayValue = false)]
+            public int ID { get; set; }
+
+            [Display(Name = "Кафедра")]
+            public string C_NAME { get; set; }
+
+            [Display(Name = "Факультет")]
+            public int DEPARTMENT_ID { get; set; }
+
+        }
+    }
+}
---
+++ KnuWeb/Controllers/CathedraController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeEF;
+
+namespace KnuWeb.Controllers
+{
+    public class CathedraController : Controller
+    {
+        private KNUDBEntities ctx = new KNUDBEntities();
+        // GET: Cathedra
+        public ActionResult Index()
+        {
+            return View(ctx.CATHEDRA.ToList());
+        }
+    }
+}
