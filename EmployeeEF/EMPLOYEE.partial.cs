34a4750462aa1afd70d9ccf96e250fd6761ece05
Igor Konobas <kois9911@gmail.com>
2018-05-16 19:25:18 +0300

    Строінку із працівниками без функціоналу додано

---
+++ EmployeeEF/EMPLOYEE.partial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeEF
+{
+    [MetadataType(typeof(EMPLOYEEMD))]
+    partial class EMPLOYEE
+    {
+        public class EMPLOYEEMD
+        {
+            [HiddenInput(DisplayValue = false)]
+            public int ID { get; set; }
+
+            [Display(Name = "Ім'я")]
+            public string NAME_E { get; set; }
+
+            [Display(Name = "Пошта")]
+            public int? EMAIL { get; set; }
+
+            [Display(Name = "Факультет")]
+            public int? DEPARTMENT { get; set; }
+
+            [Display(Name = "Кафедра")]
+            public int? CATHEDRA { get; set; }
+
+            [Display(Name = "Бал")]
+            public byte? RATING { get; set; }
+
+            [HiddenInput(DisplayValue = false)]
+            public int? PHOTO { get; set; }
+
+        }
+    }
+}
---
+++ KnuWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnuWeb.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeEF.KNUDBEntities ctx = new EmployeeEF.KNUDBEntities();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(ctx.EMPLOYEE.ToList());
+        }
+    }
+}
